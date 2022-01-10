93209acefae9d28eb851118601b331a1e4d71242
John Doe <81698889+Se8870@users.noreply.github.com>
2022-01-11 05:52:32 +0700

    making it global for readable code

--- main.py
+++ main.py
@@ -2,42 +2,40 @@ import nextcord
 import re
 import json
 
+# 2nd filtering url
 FILTER_DISCORD_LINK = r'(http[s]?:\/\/)?(www\.)?(dis)(.+[a-zA-Z0-9])\.(.+)'
 
-def main():
-    bot = nextcord.Client()
-    url_filter = re.compile(FILTER_DISCORD_LINK)
+bot = nextcord.Client()
+url_filter = re.compile(FILTER_DISCORD_LINK)
 
-    # dump json
-    with open('config.json') as f:
-        config = json.load(f)
+# dump json config
+with open('config.json') as f:
+    config = json.load(f)
 
-    @bot.event
-    async def on_ready():
-        print('Bot is ready to filter scam discord link!')
-    
-    @bot.event
-    async def on_message(ctx):
-        try:
-            extracted_url = re.search("(?P<url>https?://[^\s]+)", ctx.content).group("url")
+@bot.event
+async def on_ready():
+    print('Bot is ready to filter scam discord link!')
 
-            if extracted_url != None:
-                print(f'found the url {extracted_url}')
-                            
-                if url_filter.match(extracted_url):
-                    print(f'found regex criteria of {extracted_url}')
+@bot.event
+async def on_message(ctx):
+    try:
+        # extract the url first, before doing any check
+        extracted_url = re.search("(?P<url>https?://[^\s]+)", ctx.content).group("url")
 
-                for links in config['links']:
-                    if links == extracted_url:
-                        return
+        if extracted_url != None:
+            print(f'found the url {extracted_url}')
+                        
+            if url_filter.match(extracted_url):
+                print(f'found regex criteria of {extracted_url}')
 
-                print(f'Not in the list of good url, deleting it')
-                await ctx.delete()
-        except:
-            pass
+            for links in config['links']:
+                if links == extracted_url:
+                    return
 
-    bot.run(config['token'])
-    
+            print(f'Not in the list of good url, deleting it')
+            await ctx.delete()
+    except:
+        pass
 
 if __name__ == '__main__':
-    main()
+    bot.run(config['token'])
